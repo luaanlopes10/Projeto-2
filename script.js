df032e3f410102d2b54964b222205e1893876b2e
Luan Andrade <114778686+luaanlopes10@users.noreply.github.com>
2023-06-26 19:59:16 -0300

    First commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function toggleMode() {
   const html = document.documentElement
 
-  if (body.classList.contains("light")) {
+  if (html.classList.contains("light")) {
     html.classList.remove("light")
   } else {
     html.classList.add("light")
